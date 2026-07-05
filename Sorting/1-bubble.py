037b3bdc4cd7de5223ac21c85c97c0e7d7eb2531
MusiranaLohith <lohithkumar15p@gmail.com>
2026-07-05 22:08:23 +0530

    Added Bubble Sort implementation

---
+++ Sorting/1-bubble.py
@@ -0,0 +1,14 @@
+n = int(input("Enter the count of list: "))
+arr= []
+for i in range(0,n):
+    m = int(input())
+    arr.append(m)
+print("Before sorting: ",arr)
+
+for i in range(n-1):
+    for j in range(n-i-1):
+        if arr[j] > arr[j+1]:
+            temp = arr[j+1]
+            arr[j+1] = arr[j]
+            arr[j] = temp
+print(arr)
